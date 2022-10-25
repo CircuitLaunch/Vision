b9d7ccf977b5e26dd3bfa10838649c1d179029d1
Edward Janne <tigakub@mac.com>
2022-10-24 19:20:53 -0700

    Added startDetections() to ContentView

--- SwiftVision/SwiftVision/ContentView.swift
+++ SwiftVision/SwiftVision/ContentView.swift
@@ -67,46 +67,49 @@ struct ContentView: View {
 						// Image to display the captured frames
             Image(nsImage: nsImage)
                 .onAppear {
-                    // Get a list of attached cameras
-                    let discoveredCameraList =
-                        AVCaptureDevice.DiscoverySession(
-                            deviceTypes: [.builtInWideAngleCamera, .externalUnknown],
-                            mediaType: .video,
-                            position: .unspecified
-                        ).devices
-                    // Populate the names array and the name:id map
-                    for discovered in discoveredCameraList {
-                        cameraNames.append(discovered.localizedName)
-                        cameraIds[discovered.localizedName] = discovered.uniqueID
-                    }
-
-                    // Attach a closure to the videoCapture object  handle incoming frames
-                    videoCapture
-                        .onCapturedImage { buffer in
-                            if let buffer = buffer {
-                                // Get the dimensions of the image
-                                let width = CVPixelBufferGetWidth(buffer)
-                                let height = CVPixelBufferGetHeight(buffer)
-                                var bounds = CGRect(x: 0, y: 0, width: width, height: height)
-                                // Create a CoreImage image class with the buffer
-                                var ciImage = CIImage(cvImageBuffer: buffer)
-                                
-                                // Call method to perform detections
-                                performDetections(onImage: ciImage)
-                                
-                                // Scale the image
-                                if let scaledImage = ciImage.scaled(by: scale) {
-                                    ciImage = scaledImage
-                                    bounds.size = CGSize(width: Int(Double(width) * scale), height: Int(Double(height) * scale))
-                                }
-                                // Convert it to a CoreGraphics image and then into a Cocoa NSImage
-                                if let cgImage = sharedContext.createCGImage(ciImage, from: bounds) {
-                                    nsImage = NSImage(cgImage: cgImage, size: bounds.size)
-                                }
-                                // Update the image dimensions source of truth
-                                self.bounds = bounds
-                            }
+                        // Get a list of attached cameras
+                        let discoveredCameraList =
+                            AVCaptureDevice.DiscoverySession(
+                                deviceTypes: [.builtInWideAngleCamera, .externalUnknown],
+                                mediaType: .video,
+                                position: .unspecified
+                            ).devices
+                        // Populate the names array and the name:id map
+                        for discovered in discoveredCameraList {
+                            cameraNames.append(discovered.localizedName)
+                            cameraIds[discovered.localizedName] = discovered.uniqueID
                         }
+
+                        // Attach a closure to the videoCapture object  handle incoming frames
+                        videoCapture
+                            .onCapturedImage { buffer in
+                                if let buffer = buffer {
+                                    // Get the dimensions of the image
+                                    let width = CVPixelBufferGetWidth(buffer)
+                                    let height = CVPixelBufferGetHeight(buffer)
+                                    var bounds = CGRect(x: 0, y: 0, width: width, height: height)
+                                    // Create a CoreImage image class with the buffer
+                                    var ciImage = CIImage(cvImageBuffer: buffer)
+                                    
+                                    // Call method to perform detections
+                                    performDetections(onImage: ciImage)
+                                    
+                                    // Scale the image
+                                    if let scaledImage = ciImage.scaled(by: scale) {
+                                        ciImage = scaledImage
+                                        bounds.size = CGSize(width: Int(Double(width) * scale), height: Int(Double(height) * scale))
+                                    }
+                                    // Convert it to a CoreGraphics image and then into a Cocoa NSImage
+                                    if let cgImage = sharedContext.createCGImage(ciImage, from: bounds) {
+                                        nsImage = NSImage(cgImage: cgImage, size: bounds.size)
+                                    }
+                                    // Update the image dimensions source of truth
+                                    self.bounds = bounds
+                                }
+                            }
+                            
+                        // Call method to start detections
+                        startDetections()
                                             
                         // Start capturing
                         if let selectedId = cameraIds[selectedCamera] {
@@ -123,6 +126,11 @@ struct ContentView: View {
             .padding()
             // Shrink view to contents
             .frame(width: bounds.width)
+    }
+    
+    // Start detections
+    func startDetections() {
+        // To be implemented in the next tutorial
     }
     
     // Peform detections on image
